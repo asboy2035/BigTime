2cb242debd8f1740cc14fe68ff4daa85a631872e
ash <as_supp@icloud.com>
2025-01-02 16:08:27 +0100

    Initial Commit

---
+++ BigTime/BigTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigTimeApp.swift
+//  BigTime
+//
+//  Created by ash on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BigTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigTimeTests/BigTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BigTimeTests.swift
+//  BigTimeTests
+//
+//  Created by ash on 1/2/25.
+//
+
+import Testing
+@testable import BigTime
+
+struct BigTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
